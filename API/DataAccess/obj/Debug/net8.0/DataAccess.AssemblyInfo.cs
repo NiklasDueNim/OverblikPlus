4382270457b566806aa6b7cd40dc3a129333d4ba
NiklasNim <10481979@ucn.dk>
2024-10-08 22:58:26 +0200

    Nyt test projekt oprettet

--- API/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ API/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b52317596936954e5334df57412a04b3d9f75786")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99dcdc3c6ee80effcea23456064cabcd0890d34a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
