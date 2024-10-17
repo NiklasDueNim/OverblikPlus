879b07e8020bac47678e462eff86fba8cc21ae31
NiklasNim <10481979@ucn.dk>
2024-10-17 22:07:32 +0200

    Oprettet nye folder og tomme metoder

---
+++ API/UserService/Profile/MappingProfile.cs
@@ -0,0 +1,6 @@
+namespace UserService.Profile;
+
+public class MappingProfile
+{
+    
+}
--- API/UserService/Service/UserService.cs
+++ API/UserService/Service/UserService.cs
@@ -1,6 +1,42 @@
+using UserService.dto;
+using AutoMapper;
+using DataAccess;
+
 namespace UserService.Service;
 
-public class UserService
+public class UserService : IUserService
 {
+    private readonly AppDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public UserService(AppDbContext dbContext, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
     
+    public IEnumerable<ReadUserDto> GetAllUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ReadUserDto GetUserById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int CreateUser(CreateUserDto createUserDto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteUser(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateTask(int id, ReadUserDto readUserDto)
+    {
+        throw new NotImplementedException();
+    }
 }
