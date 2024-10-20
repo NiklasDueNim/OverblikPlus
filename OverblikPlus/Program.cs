d9387c69f80fb40d4af1d25a63ea22e75e7cf0d4
NiklasNim <10481979@ucn.dk>
2024-10-20 22:31:57 +0200

    Update Program.cs

--- OverblikPlus/Program.cs
+++ OverblikPlus/Program.cs
@@ -8,7 +8,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5165") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7237") });
 
 builder.Services.AddScoped<ITaskService, TaskService>();
 
