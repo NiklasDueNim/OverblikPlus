c0fbc42f3e74d9ecaf0d0b37c38fce0e49be4bcd
NiklasNim <10481979@ucn.dk>
2024-11-03 16:40:27 +0100

    Update ReadUserDto.cs

--- OverblikPlusBackend/UserMicroService/dto/ReadUserDto.cs
+++ OverblikPlusBackend/UserMicroService/dto/ReadUserDto.cs
@@ -4,6 +4,10 @@ public class ReadUserDto
 {
     public int Id { get; set; }
     
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
     public string Username { get; set; }
     
     public string Role { get; set; }
