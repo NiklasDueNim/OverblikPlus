5794902b3a11a972bb0289fa0cdf83f16991e47b
NiklasNim <10481979@ucn.dk>
2024-11-06 23:16:08 +0100

    Opdateret UpdateTaskStepDto

--- OverblikPlus/Dtos/TaskSteps/UpdateTaskStepDto.cs
+++ OverblikPlus/Dtos/TaskSteps/UpdateTaskStepDto.cs
@@ -2,5 +2,8 @@ namespace OverblikPlus.Dtos.TaskSteps;
 
 public class UpdateTaskStepDto
 {
+    public string Text { get; set; }
+    public string? ImageBase64 { get; set; }
+    public bool RequiresQrCodeScan { get; set; }
     
 }
