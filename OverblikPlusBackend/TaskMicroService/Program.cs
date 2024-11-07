727397e48b64e39ad2af4f5aa71a972d1333a463
NiklasNim <10481979@ucn.dk>
2024-11-08 00:23:09 +0100

    Update Program.cs

--- OverblikPlusBackend/TaskMicroService/Program.cs
+++ OverblikPlusBackend/TaskMicroService/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<TaskDbContext>(options =>
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin",
-        builder => builder.WithOrigins("https://overblikplus.dk", "http://localhost:5226", "http://localhost:5032")
+        builder => builder.WithOrigins("https://overblikplus.dk")
             .AllowAnyMethod()
             .AllowAnyHeader());
 });
