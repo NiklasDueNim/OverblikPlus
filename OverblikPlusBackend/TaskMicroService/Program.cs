b89ddcb5746dee7897d0a599cf5a84672f7c4491
NiklasNim <10481979@ucn.dk>
2024-11-17 00:06:53 +0100

    mindre rettelser i program og appsettings

--- OverblikPlusBackend/TaskMicroService/Program.cs
+++ OverblikPlusBackend/TaskMicroService/Program.cs
@@ -4,46 +4,36 @@ using Microsoft.IdentityModel.Tokens;
 using TaskMicroService.DataAccess;
 using TaskMicroService.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["ApplicationInsights:ConnectionString"]);
+
 
 builder.Services.AddDbContext<TaskDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowSpecificOrigin", builder =>
+    options.AddPolicy("AllowOverblikPlus", builder =>
         builder.WithOrigins("https://overblikplus.dk")
             .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials());
+            .AllowAnyHeader());
 });
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 builder.Services.AddControllers();
-builder.Services.AddAutoMapper(typeof(Program));  
-builder.Services.AddScoped<ITaskService, TaskService>();  
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddScoped<ITaskService, TaskService>();
 builder.Services.AddScoped<ITaskStepService, TaskStepService>();
 builder.Services.AddScoped<IImageConversionService, ImageConversionService>();
 builder.Services.AddScoped<BlobStorageService>();
 
-
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-else
-{
-    app.UseHttpsRedirection();
-}
 
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer("Bearer", options =>
@@ -60,12 +50,21 @@ builder.Services.AddAuthentication("Bearer")
         };
     });
 
+var app = builder.Build();
 
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseRouting();
-
-app.UseCors("AllowSpecificOrigin");
-
+app.UseCors("AllowOverblikPlus");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
