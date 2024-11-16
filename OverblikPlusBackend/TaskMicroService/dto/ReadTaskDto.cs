dbddada7535fe3fb94cc014fe3d198c2e4f718e9
NiklasNim <10481979@ucn.dk>
2024-11-16 20:27:39 +0100

    Opdateret fra int til string

--- OverblikPlusBackend/TaskMicroService/dto/ReadTaskDto.cs
+++ OverblikPlusBackend/TaskMicroService/dto/ReadTaskDto.cs
@@ -9,5 +9,7 @@ public class ReadTaskDto
     public string Description { get; set; }
     public List<ReadTaskStepDto>? Steps { get; set; } = new();
     public string? Image { get; set; }
-    public bool RequiresQrCodeScan { get; set; } 
+    public bool RequiresQrCodeScan { get; set; }
+
+    public string UserId { get; set; }
 }
--- OverblikPlusBackend/TaskMicroService/dto/UpdateTaskDto.cs
+++ OverblikPlusBackend/TaskMicroService/dto/UpdateTaskDto.cs
@@ -12,7 +12,7 @@ public class UpdateTaskDto
     
     public DateTime DueDate { get; set; }
 
-    public int? UserId { get; set; }
+    public string? UserId { get; set; }
 
     public bool RequiresQrCodeScan { get; set; } 
 }
