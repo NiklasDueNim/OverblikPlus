a0a736403463028c887ffdbc613da8be0206fb82
NiklasNim <10481979@ucn.dk>
2024-11-17 20:27:11 +0100

    Update Program.cs

--- OverblikPlusBackend/AuthMicroService/Program.cs
+++ OverblikPlusBackend/AuthMicroService/Program.cs
@@ -1,14 +1,28 @@
 using System.Text;
 using AuthMicroService;
+using AuthMicroService.DataAccess;
+using AuthMicroService.Entities;
+using AuthMicroService.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using AuthMicroService.DataAccess;
-using AuthMicroService.Entities;
-using AuthMicroService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorClient", policy =>
+    {
+        policy.WithOrigins("http://localhost:5226", "https://overblikplus.dk")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddDbContext<AuthDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -17,52 +31,26 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<AuthDbContext>()
     .AddDefaultTokenProviders();
 
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    });
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowOverblikPlus", corsBuilder =>
-        corsBuilder.WithOrigins("https://overblikplus.dk")
-            .AllowAnyMethod()
-            .AllowAnyHeader());
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
 builder.Services.AddScoped<IAuthService, AuthService>();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        await SeedData.InitializeRoles(services);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"An error occurred while seeding roles: {ex.Message}");
-    }
-}
+app.UseCors("AllowBlazorClient");
 
 if (app.Environment.IsDevelopment())
 {
@@ -73,10 +61,6 @@ else
 {
     app.UseHttpsRedirection();
 }
-
-app.UseRouting();
-
-app.UseCors("AllowOverblikPlus");
 
 app.UseAuthentication();
 app.UseAuthorization();
