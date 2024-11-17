b1f4df39198a404763ba4eafd0e4e314b76f173a
NiklasNim <10481979@ucn.dk>
2024-11-17 18:00:40 +0100

    Login med Email

--- OverblikPlusBackend/AuthMicroService/Services/AuthService.cs
+++ OverblikPlusBackend/AuthMicroService/Services/AuthService.cs
@@ -140,7 +140,7 @@ namespace AuthMicroService.Services
             var claims = new List<Claim>
             {
                 new Claim(JwtRegisteredClaimNames.Sub, user.Id),
-                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.Email),
                 new Claim(ClaimTypes.Email, user.Email),
                 new Claim("nameid", user.Id)
             };
