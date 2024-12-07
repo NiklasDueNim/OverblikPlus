e77ce73c61efdb4fe55e815eb401321f1b439487
NiklasNim <10481979@ucn.dk>
2024-12-07 20:58:56 +0100

    Opdateres database

---
+++ OverblikPlusBackend/TaskMicroService/Migrations/20241207195830_AddStartDate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskMicroService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStartDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
