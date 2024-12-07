bb1ae4d70bf34868570fa678c19f43bd89f3409e
NiklasNim <10481979@ucn.dk>
2024-12-07 18:44:18 +0100

    Mulighed for gentagelser ved CreateTask

--- OverblikPlus/TaskFormModel.cs
+++ OverblikPlus/TaskFormModel.cs
@@ -7,4 +7,10 @@ public class TaskFormModel
     public string Description { get; set; }
     public string ImageBase64 { get; set; }
     public string? UserId { get; set; }
+    
+    public string RecurrenceType { get; set; }
+    
+    public int RecurrenceInterval { get; set; }
+    
+    public DateTime StartDate { get; set; }
 }
