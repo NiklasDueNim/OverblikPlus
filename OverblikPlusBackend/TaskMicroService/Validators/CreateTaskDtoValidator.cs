6ba4a5bbda6b8a62145629cc26a855db6754e6f0
NiklasNim <10481979@ucn.dk>
2024-12-20 18:04:19 +0100

    Rules for CreateTaskDto

---
+++ OverblikPlusBackend/TaskMicroService/Validators/CreateTaskDtoValidator.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+using TaskMicroService.dto;
+
+namespace TaskMicroService.Validators;
+
+public class CreateTaskDtoValidator : AbstractValidator<CreateTaskDto>
+{
+    public CreateTaskDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(100).WithMessage("Name cannot exceed 100 characters.");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(500).WithMessage("Description cannot exceed 500 characters");
+    }
+}
