15d67899be71de88db7a9ccd760cdfb4234a86b7
NiklasNim <10481979@ucn.dk>
2025-01-03 14:38:16 +0100

    Update TaskFormModel.cs

--- OverblikPlus/TaskFormModel.cs
+++ OverblikPlus/TaskFormModel.cs
@@ -7,10 +7,7 @@ public class TaskFormModel
     public string Description { get; set; }
     public string ImageBase64 { get; set; }
     public string? UserId { get; set; }
-    
     public string RecurrenceType { get; set; }
-    
     public int RecurrenceInterval { get; set; }
-    
     public DateTime StartDate { get; set; }
 }
